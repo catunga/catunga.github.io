3f4e6ef58a4bc2ddd5f2ac5cd26431f28d54e4c3
Lorenzo Mello <116035795+catunga@users.noreply.github.com>
2025-02-25 23:32:19 -0300

    Add files via upload
    empece la seccion catunga y arregle algunas de la presentacion :v

--- js/index.js
+++ js/index.js
@@ -24,4 +24,47 @@ window.addEventListener('load', function() {
             imagen.classList.add('animate__animated', 'animate__fadeInUp');
         });
       });
+
+    document.querySelectorAll('.section-content').forEach(section => {
+        if (section.id !== 'section-catunga') {
+            section.style.display = 'none';
+        }
+    });
+
+    document.querySelectorAll('.seccion').forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            let sectionId = this.getAttribute('data-section');
+            let currentSection = document.querySelector('.section-content:not([style*="display: none"])');
+
+            let nextSection = document.getElementById(sectionId);
+
+
+            currentSection.scrollIntoView({ behavior: "smooth", block: "start" });
+            
+            currentSection.classList.add('animate__animated', 'animate__fadeOut');
+
+            currentSection.addEventListener('animationend', function() {
+                
+                currentSection.style.display = 'none';
+                currentSection.classList.remove('animate__animated', 'animate__fadeOut');
+
+                nextSection.style.display = 'block';
+                nextSection.classList.add('animate__animated', 'animate__fadeInUp');
+
+                nextSection.addEventListener('animationend', function() {
+                    nextSection.classList.remove('animate__animated', 'animate__fadeIn');
+                }, { once: true });
+                let sectionName = sectionId.replace('section-', '');
+                history.pushState(null, '', `#${sectionName}`);
+
+                if (sectionName === 'catunga') {
+                    encabezado.style.display = 'none';
+                } else {
+                    encabezado.style.display = 'block';
+                }
+
+            }, { once: true });
+        });
+    });
 });
