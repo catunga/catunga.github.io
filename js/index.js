75ff2a8a606b8ce888a06de41ff447ee358c913c
Lorenzo Mello <116035795+catunga@users.noreply.github.com>
2025-06-02 03:33:33 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -151,6 +151,9 @@ window.addEventListener('load', function() {
 
                 if (sectionName === 'catunga') {
                     encabezado.style.display = 'none';
+                }else if (sectionName === 'ideas') {
+                    cargarIdeas();
+                    encabezado.style.display = 'block';
                 } else {
                     encabezado.style.display = 'block';
                 }
@@ -171,6 +174,9 @@ window.addEventListener('load', function() {
 
             if (hash === 'catunga') {
                 encabezado.style.display = 'none';
+            } else if (hash === 'ideas') {
+                    cargarIdeas();
+                    encabezado.style.display = 'block';
             } else {
                 encabezado.style.display = 'block';
             }
@@ -190,6 +196,9 @@ window.addEventListener('load', function() {
 
             if (hash === 'catunga') {
                 encabezado.style.display = 'none';
+            } else if (hash === 'ideas') {
+                    cargarIdeas();
+                    encabezado.style.display = 'block';
             } else {
                 encabezado.style.display = 'block';
             }
@@ -199,6 +208,133 @@ window.addEventListener('load', function() {
 });
 
 
+document.querySelector('#goku5').addEventListener('click', function() {
+
+    this.classList.add('animate__animated', 'animate__tada');
+    
+    this.addEventListener('animationend', function() {
+
+        this.classList.remove('animate__animated', 'animate__tada');
+        
+        this.style.transition = "opacity 1s ease-out"; 
+        this.style.opacity = 0; 
+
+        this.addEventListener('transitionend', function() {
+            this.style.display = 'none';
+        });
+    }, { once: true });
+}, { once: true });
+
+
+let ideasCargadas = false;
+
+function cargarIdeas() {
 
 
 
+  const simulador = document.getElementById("section-ideas");
+  const buttons = simulador.querySelectorAll(".button");
+
+  const engine = Matter.Engine.create();
+  const world = engine.world;
+
+  const areaWidth = window.innerWidth * 0.8;
+  const areaX = window.innerWidth / 2;
+  const areaXStart = window.innerWidth * 0.1;
+
+  const groundHeight = 50;
+  const groundY = window.innerHeight - 100 + groundHeight / 2;
+
+  const ground = Matter.Bodies.rectangle(areaX, groundY, areaWidth, groundHeight, {
+    isStatic: true,
+    render: { visible: false }
+  });
+
+  const wallThickness = 50;
+  const wallHeight = window.innerHeight;
+
+  const leftWall = Matter.Bodies.rectangle(areaX - areaWidth / 2 - wallThickness / 2, window.innerHeight / 2, wallThickness, wallHeight, {
+    isStatic: true,
+    render: { visible: false }
+  });
+
+  const rightWall = Matter.Bodies.rectangle(areaX + areaWidth / 2 + wallThickness / 2, window.innerHeight / 2, wallThickness, wallHeight, {
+    isStatic: true,
+    render: { visible: false }
+  });
+
+  Matter.World.add(world, [ground, leftWall, rightWall]);
+  Matter.Resolver._restingThresh = 0.0005;
+  const buttonBodies = [];
+
+  requestAnimationFrame(() => {
+    buttons.forEach((button, index) => {
+      const rect = button.getBoundingClientRect();
+      const width = rect.width;
+      const height = rect.height;
+
+      if (width === 0 || height === 0) {
+        console.warn("Botón sin tamaño:", button);
+        return;
+      }
+
+      const x = Math.random() * (areaWidth - width) + areaXStart + width / 2;
+      const y = Math.random() * -150 - 300;
+
+      const body = Matter.Bodies.rectangle(x, y, width, height, {
+        restitution: 0.5,
+        friction: 1,
+        density: 0.01
+      });
+
+      buttonBodies.push({ body, element: button });
+      Matter.World.add(world, body);
+
+      let colorBorde = colorRandom();
+      let colorLinea = colorTransparente(colorBorde, 0.4);
+
+      button.style.border = `5px solid ${colorBorde}`;
+      button.style.transition = 'text-decoration-color 0.5s';
+
+      const uniqueClass = `custom-hover-${index}`;
+      button.classList.add(uniqueClass);
+
+      const style = document.createElement('style');
+      style.innerHTML = `
+        .${uniqueClass}:hover {
+          text-decoration-line: line-through;
+          text-decoration-thickness: 15px;
+          text-decoration-color: ${colorLinea};
+          transition: text-decoration-color 0.5s;
+        }
+      `;
+      document.head.appendChild(style);
+    });
+
+    Matter.Events.on(engine, "afterUpdate", () => {
+      buttonBodies.forEach(({ body, element }) => {
+        element.style.left = `${body.position.x - element.offsetWidth / 2}px`;
+        element.style.top = `${body.position.y - element.offsetHeight / 2}px`;
+      });
+    });
+
+    Matter.Runner.run(engine);
+  });
+
+}
+
+const paleta = ['#F3ABCB', '#FF6258', '#FFDC58', '#9DDCFF', '#5AB5E8', '#44E2F8', '#FC75FF', '#FFC9F0', '#000000', '#F384D4', 'FFC9F0', '#FFE68C', '#EECD56'];
+
+function colorTransparente(hex, alpha = 0.4) {
+  if (!hex.startsWith('#')) hex = '#' + hex;
+  const bigint = parseInt(hex.slice(1), 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
+function colorRandom(alpha = 0.4) {
+    const hex = paleta[Math.floor(Math.random() * paleta.length)];
+    return hex;
+}
